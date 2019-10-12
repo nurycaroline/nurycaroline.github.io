dad3001399fd1d68e0610268072ff7aac23718c8
nurycaroline <nurycaroline@gmail.com>
2019-10-12 13:49:35 -0300

    Init

---
+++ _next/static/6FAMqLbb73G8poQcdBAs4/pages/blog.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{BR8T:function(n,o,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog",function(){return t("YbiN")}])},YbiN:function(n,o,t){"use strict";t.r(o);var i=t("q1tI"),u=t.n(i).a.createElement;o.default=function(){return u("h1",null,"My Blog")}}},[["BR8T",1,0]]]);
