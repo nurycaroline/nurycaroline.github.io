84c224057991f6198a0a3b38bbb723564cf2ede5
nurycaroline <nurycaroline@gmail.com>
2019-10-12 15:11:38 -0300

    Ajute Scroll

--- src/containers/Works/styles.js
+++ src/containers/Works/styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Card from './Card';
+import Card from "./Card";
 
 export const Container = styled.div`
   background: linear-gradient(90deg, #45edb4 0%, #1d939e 100%);
@@ -20,11 +20,23 @@ export const List = styled.div`
   overflow-x: auto;
   padding: 20px;
 
-  /* ::-webkit-scrollbar {
-    display: none;
-  } */
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+    background-color: ${({ theme }) => theme.colors.green2};
+    height: 5px;
+  }
+  ::-webkit-scrollbar {
+    width: 12px;
+    height: 5px;
+    background-color: ${({ theme }) => theme.colors.green2};
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.colors.blue};
+    border-radius: 10px;
+    height: 4px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  }
 `;
 
 export const ListItem = styled(Card)``;
-
-
