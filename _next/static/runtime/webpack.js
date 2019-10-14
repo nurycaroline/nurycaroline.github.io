face663e58b3918ba6b00b625f0cb63aa386e8b4
nurycaroline <nurycaroline@gmail.com>
2019-10-14 20:33:51 -0300

    Atualizacao de descricoes e carregamento de estilos

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6941542bf8787b2e7018";
+/******/ 	var hotCurrentHash = "ffd48fa0baa1b4ebdf36";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/static/development/pages/index.js.de0de50682ae363ebe6d.hot-update.js
@@ -0,0 +1,57 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./src/layout/index.js":
+/*!*****************************!*\
+  !*** ./src/layout/index.js ***!
+  \*****************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Site; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _containers_Home__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../containers/Home */ "./src/containers/Home/index.js");
+/* harmony import */ var _containers_Works__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../containers/Works */ "./src/containers/Works/index.js");
+/* harmony import */ var _containers_About__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../containers/About */ "./src/containers/About/index.js");
+/* harmony import */ var _styles__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./styles */ "./src/layout/styles.js");
+var _jsxFileName = "/Users/nuriellycarolinebrizola/Desktop/workspace/Github/NuryCaroline/nurycaroline.github.io/src/layout/index.js";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+
+
+
+function Site() {
+  return __jsx(_styles__WEBPACK_IMPORTED_MODULE_4__["Container"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 10
+    },
+    __self: this
+  }, __jsx(_containers_Home__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }), __jsx(_containers_Works__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 12
+    },
+    __self: this
+  }), __jsx(_containers_About__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 13
+    },
+    __self: this
+  }));
+}
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.de0de50682ae363ebe6d.hot-update.js.map
