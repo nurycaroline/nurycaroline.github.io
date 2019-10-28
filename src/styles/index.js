31e58ac6c8f828a12f3c65d0fe56d26bb1d51367
nurycaroline <nurycaroline@gmail.com>
2019-10-28 14:52:18 -0300

    Animacao com polygon;

--- src/styles/index.js
+++ src/styles/index.js
@@ -4,16 +4,22 @@ const Animation = styled.div`
   position: absolute;
   z-index: 1;
   width: 100%;
-  height: calc(100vh / 2);
 `;
 
 export const AnimationToTop = styled(Animation)`
   animation: ${({ theme }) => theme.slideTop} 2s forwards;
+  z-index: 2;
+  height: 53vh;
   background: ${({ theme }) => theme.gradients.greenBlue270};
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0 calc(100% - 5vw));
 `;
 
 export const AnimationToBotton = styled(Animation)`
-  margin-top: calc(101vh / 2);
-  animation: ${({ theme }) => theme.slideBotton} 2s forwards;
+  z-index: 1;
+  height: 54vh;
+  bottom: 0;
   background: ${({ theme }) => theme.gradients.greenBlue270};
+  animation: ${({ theme }) => theme.slideBotton} 2s forwards;
+
+  clip-path: polygon(0 0, 100% calc(100% - 35vw), 100% 100%, 0% 100%);
 `;
