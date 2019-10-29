8d995f4c6f7981ecd14a2bcfa3bd2c3d746884a4
nurycaroline <nurycaroline@gmail.com>
2019-10-29 09:58:09 -0300

    Meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -17,10 +17,22 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <title>NuryCaroline</title>
-        <meta
-          name="Description"
-          content="Portfólio de Nurielly Caroline Brizola"
-        ></meta>
+        <meta name="description" content="Nurielly\'s portfolio"/>
+        <meta name="keywords" content="NURY, NURIELLY, NURYCAROLINE, DEVELOPER, FRONT, FRONT-END, DESENVOLVIMENTO, MOBILE, REACT, JAVASCRIPT, REACT NATIVE, RN, HTML, CSS, JS" />
+        <meta name="language" content="English" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:url" content="https://nurielly.dev/" />
+        <meta property="og:title" content="Nurielly Caroline Brizola" />
+        <meta property="og:site_name" content="NuryCaroline" />
+        <meta property="og:description" content="Nurielly's portfolio" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/static/og-image.png"/>
+        <meta property="og:image:type" content="image/jpeg"/>
+        <meta property="og:image:width" content="800"/>
+        <meta property="og:image:height" content="600"/>
+
         <Head>
           <link
             rel="shortcut icon"
