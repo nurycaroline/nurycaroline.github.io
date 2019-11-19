cc850a8a5b13b4b80d7fe54fb5d573cfcf198b84
nurycaroline <nurycaroline@gmail.com>
2019-11-19 11:37:38 -0300

    Add script hotjar

--- pages/_document.js
+++ pages/_document.js
@@ -49,6 +49,16 @@ export default class MyDocument extends Document {
             type="image/png"
             sizes="32x32"
           />
+          <script>
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:1578346,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          </script>
           {this.props.styleTags}
         </Head>
         <body>
